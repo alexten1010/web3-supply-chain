f0dd1d8c02400630bbfcd8cc56477943adb44480
Alex Ten <alexten3517@gmail.com>
2024-06-13 19:51:09 -0400

    Update index.js

--- Images/index.js
+++ Images/index.js
@@ -1,6 +1,6 @@
 import shipCount from "./1.jpg";
 import getShipment from "./2.jpg";
-import compShipment from "./shipment.jpg";
+import shipmentcomp from "./shipment.jpg";
 import startShipment from "./4.jpg";
 import userProfile from "./5.jpg";
 import send from "./6.jpg";

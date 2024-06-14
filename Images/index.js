35ffc4a68e024feca92d5224ea7acd09f9468d8b
Alex Ten <alexten3517@gmail.com>
2024-06-13 20:58:42 -0400

    Update index.js
    organized index file.

--- Images/index.js
+++ Images/index.js
@@ -1,17 +1,19 @@
-import shipCount from "./1.jpg";
-import getShipment from "./2.jpg";
+import userProfile  from "./1.jpg";
+import startShipment from "./2.jpg";
 import compShipment from "./3.jpg";
-import startShipment from "./4.jpg";
-import userProfile from "./5.jpg";
-import send from "./6.jpg";
+import getShipment from "./4.jpg";
+import send from "./5.jpg";
+import shipCount from "./6.jpg";
 import avatar from "./creator9.png";
 
 export default {
-  shipCount,
-  getShipment,
-  compShipment,
-  startShipment,
+
   userProfile,
+  startShipment,
+  compShipment,
+  getShipment,
   send,
+  shipCount,
   avatar,
-};
+  
+ };
